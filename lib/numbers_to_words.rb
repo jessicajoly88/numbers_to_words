d9dfccc241343bd88f07b06e888c1fb31e46642c
Jessica Joly <jessicajoly88@gmail.com>
2015-08-04 16:21:50 -0700

    Commit for integers from 0 to 20

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,7 +1,8 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    ones={1=>'one',2=>'two',3=>'three',4=>'four',5=>'five',6=>'six',7=>'seven',8=>'eight',9=>'nine'}
-    number=ones.fetch(self)
+    one_20={1=>'one',2=>'two',3=>'three',4=>'four',5=>'five',6=>'six',7=>'seven',8=>'eight',9=>'nine',10=>'ten',11=>'eleven',12=>'twelve',13=>'thirteen',14=>'fourteen',15=>'fifteen',16=>'sixteen',
+    17=>'17',18=>'eighteen',19=>'nineteen',20=>'twenty'}
+    number=one_20.fetch(self)
     return number
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -6,4 +6,7 @@ describe('Fixnum#numbers_to_words') do
   it('changes single digit integers into strings') do
     expect((1).numbers_to_words()).to(eq("one"))
   end
+  it('changes double digit integers from 10 to 20 into strings') do
+    expect((10).numbers_to_words()).to(eq("ten"))
+  end
 end
